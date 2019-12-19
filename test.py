b8d17db7c76c2fc4eb3e13f7f4671c6091489a6f
Ren Jie Kan <renjie.kan@mail.utoronto.ca>
2019-12-18 20:29:40 -0500

    made an entity class that represents albums

---
+++ test.py
@@ -0,0 +1,76 @@
+import os
+import sys 
+import json 
+import spotipy
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+#Get username from terminal
+
+#User id: 31ytkldamhg43ewe6yaspk3lzhvu 
+#ClientId: e13d2298f41947c6ad924c351d613c97
+#ClientSecret: 9b49daab7e6747b9b2fee937fc1dc672
+#RediretURI: https://github.com/TomKan0909
+
+USER_ID = '31ytkldamhg43ewe6yaspk3lzhvu' 
+
+def current_user_albums_artist(sp):
+    results = sp.current_user_saved_albums(limit=50,offset=0)
+    albums = results
+    i = 0 
+    while results['items'] != []:
+        i += 50
+        results = sp.current_user_saved_albums(limit=50,offset=i)
+        albums.update(results)
+    # return sp.current_user_saved_albums(50) # 50 is max. albums return at once
+    return albums
+
+
+def _cur_usr_alb_help(js, albums):
+    for item in js['items']:
+        artist = item["album"]["artists"]["name"]
+        if artist not in albums:
+            albums[artist] = {} # albums of artist; formart (nameOfAlbum : albumURI)
+        
+    pass
+
+
+
+def main():
+    try:
+        token = util.prompt_for_user_token(USER_ID, 'user-library-read')
+    except(AttributeError):
+        os.remove(f".cache-{USER_ID}") 
+        token = util.prompt_for_user_token(USER_ID, 'user-library-read') 
+
+
+    sp = spotipy.Spotify(auth=token)
+
+    
+    result = current_user_albums_artist(sp)
+    # result = json.dumps(sp.current_user_saved_albums(limit=1), sort_keys= True, indent=4)
+    # albums = {}
+    # for album in result:
+        
+    # result = current_user_albums(sp)
+    f = open('text.txt', 'w')
+    for line in result:
+        f.write(line)
+
+
+
+if __name__ == "__main__":
+    main()
+
+# # Get artist image
+# artist = input("Enter artist: ")
+
+# results = sp.search(q='artist:' + artist, type='artist')
+# items = results['artists']['items']
+# if len(items) > 0:
+#     artist = items[0]
+#     print (artist['name']) 
+#     print ('---------------------------------')
+#     for i in range(len(artist['images'])):
+#         print (artist['images'][i]['url']) # ['url']
