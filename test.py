c6e449c30e340d8413c3bebd06557d520aec8eae
Ren Jie Kan <renjie.kan@mail.utoronto.ca>
2019-12-30 19:32:13 -0500

    take user input and play selected artist

--- test.py
+++ test.py
@@ -8,11 +8,7 @@ from json.decoder import JSONDecodeError
 from spotify_album import SpotifyAlbum
 from spotify_artist import SpotifyArtist
 
-"""TODO: 1. Extract user's saved albums info q
 
-"""
-
-#Get username from terminal
 
 
 
@@ -33,6 +29,7 @@ def current_user_albums_artist(sp):
     # return sp.current_user_saved_albums(50) # 50 is max. albums return at once
     return albums
 
+"""helper method for current_user_albums_artists"""
 def _current_user_album_artist_helper(results):
     albums = {}
     for item in results['items']:
@@ -48,19 +45,49 @@ def _current_user_album_artist_helper(results):
 def main():
     USER_ID = '31ytkldamhg43ewe6yaspk3lzhvu' 
     play_token = None
+    read_token = None
     try:
-        play_token = util.prompt_for_user_token(USER_ID, 'user-library-read')
+        read_token = util.prompt_for_user_token(USER_ID, 'user-library-read')
+        play_token = util.prompt_for_user_token(USER_ID, 'user-modify-playback-state')
     except(AttributeError):
         os.remove(f".cache-{USER_ID}") 
-        play_token = util.prompt_for_user_token(USER_ID, 'user-library-read')
+        read_token = util.prompt_for_user_token(USER_ID, 'user-library-read')
+        play_token = util.prompt_for_user_token(USER_ID, 'user-modify-playback-state')
 
 
 
-    sp = spotipy.Spotify(auth=play_token)
-    # sp.start_playback();
+    sp_read = spotipy.Spotify(auth=read_token)
+    sp_play = spotipy.Spotify(auth=play_token)
+    # sp_play.start_playback(context_uri='spotify:album:1vWnB0hYmluskQuzxwo25a')
     
-    print(sp.current_user()["display_name"])
-    print(current_user_albums_artist(sp))
+    #Get artists albums dictionary
+    usr_albums = current_user_albums_artist(sp_read)
+    #Get artist_id
+    artists = list(usr_albums.keys())
+    #Lambda function to turn artist id into strings
+    artist_name = lambda artist_id: (sp_read.artist(artist_id))["name"]
+    artists_name = [artist_name(artist) for artist in artists]
+    artists_name_list = list(enumerate(artists_name, 1))
+
+    for item in artists_name_list:
+        print(item)
+
+    usr_input = int(input("pick an artist by entering number between 1 and {}: "  
+                    .format(len(artists_name_list))))
+    
+    if usr_input < 1 or usr_input > len(artists_name_list):
+        usr_input = input("pick an artist by entering number between 1 and {}: "
+                        .format(len(artists_name_list))) 
+    else:
+        picked_artist = artists[usr_input - 1]
+        picked_artists_context_uri = sp_read.artist(picked_artist)["uri"]
+        sp_play.start_playback(context_uri=picked_artists_context_uri)
+        print("Currently playing {}".format(artist_name(picked_artist)))
+
+
+    
+
+
 
         
 
